441fd0a03409017cddee24d168039db2c0926cce
Dennis Bailey <dennis.bailey@zayo.com>
2020-09-28 17:07:06 -0400

    Convert to "Pet" component to JSX

--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 
-export default function Pet({
-    name,
-    animal,
-    breed
-}) {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-    ]);
-};
+export default function Pet({ name, animal, breed }) {
+    // return React.createElement("div", {}, [
+    //     React.createElement("h1", {}, name),
+    //     React.createElement("h2", {}, animal),
+    //     React.createElement("h2", {}, breed),
+    // ]);
+
+    // This is the JSX implementation of the React above. It will be transpiled to what we see above
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h2>{animal}</h2>
+            <h2>{breed}</h2>
+        </div>
+    );
+}
