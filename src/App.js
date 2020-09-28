306350e4f97fc723de451f8da596b45144ab4845
Dennis Bailey <dennis.bailey@zayo.com>
2020-09-28 12:44:47 -0400

    Create a "Pet" component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-// Your code goes here (for now)
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Sammy"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h2", {}, "Mutt"),
+    ]);
+}
 const App = () => {
     return React.createElement(
         "div", // What kind of element is it?
         {}, // Component attributes that will be passed into the child element. ex: { id: "important- id"}
-        React.createElement("h1", {}, "Adopt Me!") // Singular child or an array of children [React.createElement("h1", {}, "Adopt Me!"), React.createElement("h1", {}, "Adopt Me!")]
+        // React.createElement("h1", {}, "Adopt Me!") // Singular child or an array of children [React.createElement("h1", {}, "Adopt Me!"), React.createElement("h1", {}, "Adopt Me!")]
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {}, []), // createElement(Pet, {}, []) is the same as createElement(Pet)
+            React.createElement(Pet),
+            React.createElement(Pet)
+        ]
     );
 };
 
