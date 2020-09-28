72a1706cb6712ce10a92a4b8dda0414f5229ad31
Dennis Bailey <dennis.bailey@zayo.com>
2020-09-28 12:49:32 -0400

    Use props to define Pet component values

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Sammy"),
-        React.createElement("h2", {}, "Dog"),
-        React.createElement("h2", {}, "Mutt"),
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
     ]);
 }
 const App = () => {
     return React.createElement(
         "div", // What kind of element is it?
         {}, // Component attributes that will be passed into the child element. ex: { id: "important- id"}
-        // React.createElement("h1", {}, "Adopt Me!") // Singular child or an array of children [React.createElement("h1", {}, "Adopt Me!"), React.createElement("h1", {}, "Adopt Me!")]
         [
             React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {}, []), // createElement(Pet, {}, []) is the same as createElement(Pet)
-            React.createElement(Pet),
-            React.createElement(Pet)
-        ]
+            React.createElement(Pet, { name: "Sammy", animal: "Dog", breed: "Mutt" }), // createElement(Pet, {}, []) is the same as createElement(Pet)
+            React.createElement(Pet, { name: "Jimmy", animal: "Cat", breed: "Orange Marmalade" }),
+            React.createElement(Pet, { name: "Dylan", animal: "Dog", breed: "Yellow Lab" })
+        ] // Singular child or an array of children
     );
 };
 
