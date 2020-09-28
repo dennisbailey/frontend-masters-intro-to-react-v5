36b0a2c504727d248d73b9b56b07943e6ce1864b
Dennis Bailey <dennis.bailey@zayo.com>
2020-09-28 17:27:00 -0400

    Remove commented out code

--- src/App.js
+++ src/App.js
@@ -3,28 +3,6 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-    // return React.createElement(
-    //     "div", // What kind of element is it?
-    //     {}, // Component attributes that will be passed into the child element. ex: { id: "important- id"}
-    //     [
-    //         React.createElement("h1", {}, "Adopt Me!"),
-    //         React.createElement(Pet, {
-    //             name: "Sammy",
-    //             animal: "Dog",
-    //             breed: "Mutt",
-    //         }), // createElement(Pet, {}, []) is the same as createElement(Pet)
-    //         React.createElement(Pet, {
-    //             name: "Jimmy",
-    //             animal: "Cat",
-    //             breed: "Orange Tabby",
-    //         }),
-    //         React.createElement(Pet, {
-    //             name: "Dylan",
-    //             animal: "Dog",
-    //             breed: "Yellow Lab",
-    //         }),
-    //     ] // Singular child or an array of children
-    // );
     return (
         <div>
             <h1>Adopt Me!</h1>
--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,6 @@
 import React from "react";
 
 export default function Pet({ name, animal, breed }) {
-    // return React.createElement("div", {}, [
-    //     React.createElement("h1", {}, name),
-    //     React.createElement("h2", {}, animal),
-    //     React.createElement("h2", {}, breed),
-    // ]);
-
-    // This is the JSX implementation of the React above. It will be transpiled to what we see above
     return (
         <div>
             <h1>{name}</h1>
