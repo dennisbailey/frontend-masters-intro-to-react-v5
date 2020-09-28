ef66ba01b81bb852f91a6d0ba697b005ae33d138
Dennis Bailey <dennis.bailey@zayo.com>
2020-09-28 17:24:44 -0400

    Convert App.js to JSX

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import {
-    render
-} from 'react-dom';
-import Pet from './Pet';
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-    return React.createElement(
-        "div", // What kind of element is it?
-        {}, // Component attributes that will be passed into the child element. ex: { id: "important- id"}
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Sammy",
-                animal: "Dog",
-                breed: "Mutt",
-            }), // createElement(Pet, {}, []) is the same as createElement(Pet)
-            React.createElement(Pet, {
-                name: "Jimmy",
-                animal: "Cat",
-                breed: "Orange Marmalade",
-            }),
-            React.createElement(Pet, {
-                name: "Dylan",
-                animal: "Dog",
-                breed: "Yellow Lab",
-            }),
-        ] // Singular child or an array of children
+    // return React.createElement(
+    //     "div", // What kind of element is it?
+    //     {}, // Component attributes that will be passed into the child element. ex: { id: "important- id"}
+    //     [
+    //         React.createElement("h1", {}, "Adopt Me!"),
+    //         React.createElement(Pet, {
+    //             name: "Sammy",
+    //             animal: "Dog",
+    //             breed: "Mutt",
+    //         }), // createElement(Pet, {}, []) is the same as createElement(Pet)
+    //         React.createElement(Pet, {
+    //             name: "Jimmy",
+    //             animal: "Cat",
+    //             breed: "Orange Tabby",
+    //         }),
+    //         React.createElement(Pet, {
+    //             name: "Dylan",
+    //             animal: "Dog",
+    //             breed: "Yellow Lab",
+    //         }),
+    //     ] // Singular child or an array of children
+    // );
+    return (
+        <div>
+            <h1>Adopt Me!</h1>
+            <Pet name="Sammy" animal="Dog" breed="Mutt" />
+            <Pet name="Jimmy" animal="Cat" breed="Orange Tabby" />
+            <Pet name="Dylan" animal="Dog" breed="Yellow Lab" />
+        </div>
     );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
