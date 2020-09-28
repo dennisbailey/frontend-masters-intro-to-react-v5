3e7ac1a86f4da79b0f1d94a9db1574d1bb351be8
Dennis Bailey <dennis.bailey@zayo.com>
2020-09-28 17:34:10 -0400

    Create "SearchParams" component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
     return (
         <div>
             <h1>Adopt Me!</h1>
-            <Pet name="Sammy" animal="Dog" breed="Mutt" />
-            <Pet name="Jimmy" animal="Cat" breed="Orange Tabby" />
-            <Pet name="Dylan" animal="Dog" breed="Yellow Lab" />
+            <SearchParams />
         </div>
     );
 };
