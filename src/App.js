f9c28c56041fc56243a96f83fb9737de737be05d
Dennis Bailey <dennis.bailey@zayo.com>
2020-09-28 13:14:25 -0400

    Create and apply .prettierrc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({
+    name,
+    animal,
+    breed
+}) => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, name),
         React.createElement("h2", {}, animal),
         React.createElement("h2", {}, breed),
     ]);
 };
+
 const App = () => {
     return React.createElement(
         "div", // What kind of element is it?
