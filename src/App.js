3521852066e1b2709bc8f6df2881480754730f18
Dennis Bailey <dennis.bailey@zayo.com>
2020-09-28 13:09:06 -0400

    Apply prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-const Pet = ({name, animal, breed}) => {
+const Pet = ({ name, animal, breed }) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {},name),
-        React.createElement("h2", {},animal),
-        React.createElement("h2", {},breed),
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed),
     ]);
-}
+};
 const App = () => {
     return React.createElement(
         "div", // What kind of element is it?
         {}, // Component attributes that will be passed into the child element. ex: { id: "important- id"}
         [
             React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, { name: "Sammy", animal: "Dog", breed: "Mutt" }), // createElement(Pet, {}, []) is the same as createElement(Pet)
-            React.createElement(Pet, { name: "Jimmy", animal: "Cat", breed: "Orange Marmalade" }),
-            React.createElement(Pet, { name: "Dylan", animal: "Dog", breed: "Yellow Lab" })
+            React.createElement(Pet, {
+                name: "Sammy",
+                animal: "Dog",
+                breed: "Mutt",
+            }), // createElement(Pet, {}, []) is the same as createElement(Pet)
+            React.createElement(Pet, {
+                name: "Jimmy",
+                animal: "Cat",
+                breed: "Orange Marmalade",
+            }),
+            React.createElement(Pet, {
+                name: "Dylan",
+                animal: "Dog",
+                breed: "Yellow Lab",
+            }),
         ] // Singular child or an array of children
     );
 };
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
