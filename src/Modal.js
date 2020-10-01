e5830df35877f3940f5d34fb56c6b1db1bebf7d0
Dennis Bailey <dennis.bailey@zayo.com>
2020-10-01 12:51:59 -0600

    Create "Modal"

---
+++ src/Modal.js
@@ -0,0 +1,22 @@
+import Reac, { useEffect, useRef } from "react;";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+    const elRef = useRef(null);
+
+    if (!elRef.current) {
+        const div = document.createElement("div");
+        elRef.current = div;
+    }
+
+    useEffect(() => {
+        const modalRoot = docuemnt.getElementById("modal");
+        modalRoot.appendChild(elRef.current);
+
+        return () => modalRoot.removeChild(elRef.current);
+    }, []); // No dependencies, only run once
+
+    return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
